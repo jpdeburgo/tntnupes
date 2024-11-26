bdfcc2388cd27162545b12da269bbec34fb1b676
Justin De Burgo <jpdeburgo.jp@gmail.com>
2024-11-26 11:33:34 -0600

    adding readme

---
+++ utils/sample.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "APIKEY",
+  authDomain: "AUTHDOMAIN",
+  projectId: "PROJECTID",
+  storageBucket: "STORAGEBUCKET",
+  messagingSenderId: "MESSAGINGSENDERID",
+  appId: "APPID",
+  measurementId: "MEASUREMENTID",
+};
