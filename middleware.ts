653c74a48f45812899c1ea1d271a01a09053ae21
Justin De Burgo <jpdeburgo.jp@gmail.com>
2024-12-15 16:47:08 -0600

    adding try catch

--- middleware.ts
+++ middleware.ts
@@ -16,22 +16,31 @@ export async function middleware(req: NextRequest) {
   const authUser = cookieList.get("authUser");
   const { uid } = JSON.parse(authUser.value);
 
-  // Make an API call to verify user status
-  const res = await fetch(`${req.nextUrl.origin}/api/verify-user`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ uid }), // Replace with actual UID logic
-  });
+  try {
+    // Make an API call to verify user status
+    const res = await fetch(`${req.nextUrl.origin}/api/verify-user`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ uid }), // Replace with actual UID logic
+    });
 
-  if (res.ok) {
-    const text = await res.text(); // Use text() instead of json() to inspect the raw response
-    const data = JSON.parse(text);
-    console.log({ text, data });
-    if (data.user?.verified) {
-      return NextResponse.next();
+    if (res.ok) {
+      const text = await res.text(); // Use text() instead of json() to inspect the raw response
+      const data = JSON.parse(text);
+      const { authUser, user } = data;
+      console.log({ text, data });
+      if (user?.verified) {
+        return NextResponse.next();
+      } else if (authUser && user && !user?.verified) {
+        return NextResponse.redirect(
+          new URL("/waiting-for-verification", req.url)
+        );
+      }
     }
+  } catch (error) {
+    console.error(error);
   }
 
   // Redirect unauthorized users to /signin
--- pages/api/verify-user.ts
+++ pages/api/verify-user.ts
@@ -4,8 +4,11 @@ import admin from "../../utils/firebaseAdmin";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const { uid } = req.body; // Example: Expecting a user ID in the request body
-    const user = await admin.auth().getUser(uid); // Admin SDK action
-    res.status(200).json({ user });
+    const authUser = await admin.auth().getUser(uid); // Admin SDK action
+    const user = (
+      await admin.firestore().collection("users").doc(uid).get()
+    )?.data(); // Admin SDK action
+    res.status(200).json({ authUser, user });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Failed to verify user." });
